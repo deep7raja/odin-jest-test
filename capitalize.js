2dafd89e0fd26c33a4af18eaee2d3bddc957effc
deep7raja <56893558+deep7raja@users.noreply.github.com>
2022-02-08 19:13:26 +0530

    capitalize done

--- capitalize.js
+++ capitalize.js
@@ -1,3 +1,10 @@
 export function capitalize(str){
-    return 'Hello';
+    if(str.length<1){
+        return '';
+    }
+    let ans = str.charAt(0).toUpperCase();
+    for(let i=1; i<str.length; ++i){
+        ans += str.charAt(i);
+    }
+    return ans;
 }
--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,27 @@ import { capitalize } from "./capitalize"
 test('basic check', ()=>{
     expect(capitalize('hello')).toBe('Hello');
 })
+
+test('more basic check', ()=>{
+    expect(capitalize('there')).toBe('There');
+})
+
+test('Already capital', ()=>{
+    expect(capitalize('Hello')).toBe('Hello');
+})
+
+test('empty input check', ()=>{
+    expect(capitalize('')).toBe('');
+})
+
+test('string with space', ()=>{
+    expect(capitalize("hello there")).toBe("Hello there");
+})
+
+test('numbered first charactor', ()=>{
+    expect(capitalize('4hello')).toBe('4hello');
+})
+
+test('special first charactor', ()=>{
+    expect(capitalize('*hello')).toBe('*hello');
+})
